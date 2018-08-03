6409753a80e94d25edbfb6c898d2b8010c5458af
sibyldawn <sibyldawn@gmail.com>
2018-08-02 17:46:27 -0700

    Add Tabs for Dashboard

--- src/Components/Components/Products.js
+++ src/Components/Components/Products.js
@@ -4,7 +4,36 @@ import { Grid,Row,Col,Button,Media} from 'react-bootstrap'
 export default class Products extends Component {
     render() {
         return (
-            <>
+            <div>
+             <Media>
+                <Media.Left>
+                <img width={64} height={64} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhaM7wMcPIMCYEbmg2fZCcD5mKpTDYDu8Hj53pN0klqnUM0lZq"  alt="thumbnail" />
+                </Media.Left>
+                <Media.Body>
+                <Media.Heading>Media Heading</Media.Heading>
+                <p>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                    ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                    tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                    fringilla. Donec lacinia congue felis in faucibus.
+                </p>
+                </Media.Body>
+            </Media>
+            <Media>
+                <Media.Left>
+                <img width={64} height={64} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhaM7wMcPIMCYEbmg2fZCcD5mKpTDYDu8Hj53pN0klqnUM0lZq"  alt="thumbnail" /> alt="thumbnail" />
+                </Media.Left>
+                <Media.Body>
+                <Media.Heading>Media Heading</Media.Heading>
+                <p>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                    ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                    tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                    fringilla. Donec lacinia congue felis in faucibus.
+                </p>
+                </Media.Body>
+              </Media>
+            </div>
         );
     }
 }
--- src/Components/Pages/Admin_Dashboard.js
+++ src/Components/Pages/Admin_Dashboard.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import {Tabs,Tab} from 'react-bootstrap';
 import './Admin_Dashboard.css';
+import Products from '../Components/Products';
+import Orders from '../Components/Orders';
+import Add_Form from '../Components/Add_Form';
 
 export default class AdminDashboard extends Component {
     render() {
-        return ( <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
+        return (
+        <div className="dashboard">    
+        <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
         <Tab eventKey={1} title="Tab 1">
-          Tab 1 content
+          <Products/>
         </Tab>
         <Tab eventKey={2} title="Tab 2">
-          Tab 2 content
+          <Orders/>
         </Tab>
-        <Tab eventKey={3} title="Tab 3" disabled>
-          Tab 3 content
+        <Tab eventKey={3} title="Tab 3" >
+          <Add_Form/>
         </Tab>
       </Tabs>
+      </div>
         )
     }
 }
