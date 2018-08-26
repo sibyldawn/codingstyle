2cc3ce9c750d7287df9da0a326add0a7500a1da8
sibyldawn <sibyldawn@gmail.com>
2018-08-25 21:29:08 -0700

    Imported Redirect

--- src/Components/Pages/ShoppingBag/ShoppingBag.js
+++ src/Components/Pages/ShoppingBag/ShoppingBag.js
@@ -14,6 +14,7 @@ import ButtonBase from '@material-ui/core/ButtonBase';
 import Button from '@material-ui/core/Button';
 import { Redirect } from 'react-router-dom'; 
 import TextField from '@material-ui/core/TextField';
+import { Redirect } from 'react-router-dom';
 import {updateNotAdmin, updateAdmin, updateUser, updateLogin, updateTotal, updateCart} from '../../../ducks/reducer';
 
 
